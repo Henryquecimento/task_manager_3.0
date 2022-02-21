27dcfb914d646970e4463f25e6db6615884c83b0
henryque <henryque_1@hotmail.com>
2022-02-21 18:57:46 -0400

    refactor: readjust CreateUsersToDo

--- src/functions/CreateUsersToDo.ts
+++ src/functions/CreateUsersToDo.ts
@@ -1,18 +1,38 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
-import { document } from "src/utils/dynamoClient";
+import { document } from "../utils/dynamoClient";
+import { v4 as uuid } from "uuid";
 
 interface ICreateUserToDo {
+  title: string;
+  deadline: Date;
+}
+
+interface ITemplate {
   id: string;
   user_id: string;
   title: string;
   done: boolean;
-  deadline: string;
+  deadline: Date;
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-  const { id, user_id, title, done, deadline } = JSON.parse(
-    event.body
-  ) as ICreateUserToDo;
+  const { user_id } = event.pathParameters;
+  const { title, deadline } = JSON.parse(event.body) as ICreateUserToDo;
+
+  const id = uuid();
+
+  await document
+    .put({
+      TableName: "user_todos",
+      Item: {
+        id,
+        user_id,
+        title,
+        done: false,
+        deadline: new Date(deadline).getTime(),
+      },
+    })
+    .promise();
 
   const response = await document
     .query({
@@ -24,28 +44,8 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     })
     .promise();
 
-  const toDoAlreadyExists = response.Items[0];
-
-  if (!toDoAlreadyExists) {
-    await document
-      .put({
-        TableName: "user_todos",
-        Item: {
-          id,
-          user_id,
-          title,
-          done,
-          deadline: new Date(deadline),
-        },
-      })
-      .promise();
-  }
-
   return {
     statusCode: 201,
-    body: JSON.stringify({
-      message: "To Do Created",
-      toDo: response.Items[0],
-    }),
+    body: JSON.stringify(response.Items[0] as ITemplate),
   };
 };
